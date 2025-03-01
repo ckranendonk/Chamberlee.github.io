2690a77dffda87ae648b3070ce01608676baf966
ckranendonk <ckranendonk5@gmail.com>
2025-03-01 10:32:57 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const greeting = document.getElementById ("greeting");
+greeting.style.textAlign = "center";
+greeting.style.marginTop = "50px";
+const username =window.prompt("What is your name?", "Jane");
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
